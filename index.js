2e64b28ae69ba71af4650c6fd0dc8bce32e3a9c8
Marcus Schnake <marcusschnake@Marcuss-MBP.lan>
2021-10-07 21:04:52 -0400

    linking divs

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+
+
+// async function fetchPokemon() {
+//     const randNum = Math.floor(Math.random() * (151 - 2) + 2)
+//     const response = await fetch(`${baseURL}${randNum}`);
+//     console.log(response);
+//     const json = await response.json();
+//     displayPokemon(json)
+//     console.log(json)
+    
+// }
