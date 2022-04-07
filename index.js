353169cdb3f083560cdb0c0361286355292e55b8
Marcus A. Schnake <marcus.schnake@gmail.com>
2022-04-06 23:14:39 -0400

    working on modal

--- index.js
+++ index.js
@@ -2,12 +2,3 @@
 
 
 
-// async function fetchPokemon() {
-//     const randNum = Math.floor(Math.random() * (151 - 2) + 2)
-//     const response = await fetch(`${baseURL}${randNum}`);
-//     console.log(response);
-//     const json = await response.json();
-//     displayPokemon(json)
-//     console.log(json)
-    
-// }
